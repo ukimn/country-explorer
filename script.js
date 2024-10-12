a7d5047acf76adad296667d6d1c3ee17f0a79dd3
moaath ibrahim <moaaz@gmail.com>
2024-10-12 10:36:22 -0700

    Just Started

---
+++ script.js
@@ -0,0 +1,26 @@
+async function fetchData() {
+    try {
+        const flagName = document.getElementById("textBox").value.toLowerCase();
+
+        const response = await fetch(`https://restcountries.com/v3.1/name/${flagName}`);
+
+        if (!response.ok) {
+            throw new Error("Could not fetch source")
+        } else {
+            const data = await response.json();
+
+            const country = data[0];
+            const flagImg = country.flags.png; 
+
+            const imgElement = document.getElementById('images');
+            imgElement.src = flagImg;
+            imgElement.style.display = "block"
+
+            console.log(country)
+        }
+
+    } catch (err) {
+        console.error(err);
+    }
+}
+
